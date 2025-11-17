7b109f191d1c37a572ae1a9abc44ef260e64fdf6
souketsu <sou_ketsu@foxmail.com>
2025-11-17 18:47:58 +0800

    完成绘画和重置功能

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
 const BOARD_SIZE = 16;
 const etchDiv = document.querySelector('#etchDiv');
+const resetButton = document.querySelector('#reset-btn');
 
 // 生成像素画板网格
 function createEtchBoard() {
-    for (let index = 0; index < BOARD_SIZE; index++) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixel.addEventListener('mousedown',colorPixel);
-        pixel.addEventListener('mouseover',colorPixelIfDragging);
-        etchDiv.appendChild(pixel);
+    // 清空旧网格
+    etchDiv.innerHTML = '';
+    for (let createRow = 0; createRow < BOARD_SIZE; createRow++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        
+        for (let index = 0; index < BOARD_SIZE; index++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            pixel.addEventListener('mousedown',colorPixel);
+            pixel.addEventListener('mouseover',colorPixelIfDragging);
+            row.appendChild(pixel);
+            
+        }  
+        etchDiv.appendChild(row);
     }
 }
 
 // 绘画功能
 function colorPixel(e) {
     e.target.classList.add('drawn');
+    e.preventDefault();
 }
 
 // 绘画功能-拖动
@@ -23,3 +34,11 @@ function colorPixelIfDragging(e) {
         e.target.classList.add('drawn');
     }
 }
+
+// 重置功能
+function resetBoard() {
+    createEtchBoard();
+}
+
+document.addEventListener('DOMContentLoaded',createEtchBoard);
+resetButton.addEventListener('click',resetBoard);
